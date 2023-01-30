e187c96f08368640d3b3b6141be6202c98c6e9d9
TamakiMaeda <giraph.me@gmail.com>
2023-01-30 14:09:47 +0900

    setup interface of request

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import http, { IncomingMessage, ServerResponse } from "http";
 
+import { generateButton } from "./generateButton";
+
 const server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -7,8 +9,21 @@ const server = http.createServer(
     res.setHeader("Access-Control-Allow-Methods", "*");
     res.setHeader("Access-Control-Allow-Headers", "*");
     res.setHeader("Content-Type", "application/json");
-    res.end('{"result": true}'); // レスポンスボディが「OK」になる
+    const result = handleRequest(req);
+    res.end(JSON.stringify(result));
   }
 );
+
+const handleRequest = (
+  req: IncomingMessage
+): ReturnType<typeof generateButton | typeof notFoundHandler> => {
+  if (req.method === "get" && req.url === "/generate-button") {
+    return generateButton();
+  }
+
+  return notFoundHandler();
+};
+
+const notFoundHandler = () => ({ result: false });
 
 server.listen(4000);
