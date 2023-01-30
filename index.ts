9815797c8046940edc74e5d1694f0f3ba5a68f72
TamakiMaeda <giraph.me@gmail.com>
2023-01-30 14:28:23 +0900

    parameter generator

--- index.ts
+++ index.ts
@@ -3,27 +3,43 @@ import http, { IncomingMessage, ServerResponse } from "http";
 import { generateButton } from "./generateButton";
 
 const server = http.createServer(
-  (req: IncomingMessage, res: ServerResponse) => {
+  async (req: IncomingMessage, res: ServerResponse) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Request-Method", "*");
     res.setHeader("Access-Control-Allow-Methods", "*");
     res.setHeader("Access-Control-Allow-Headers", "*");
     res.setHeader("Content-Type", "application/json");
-    const result = handleRequest(req);
+    const result = await handleRequest(req);
     res.end(JSON.stringify(result));
   }
 );
 
-const handleRequest = (
+const handleRequest = async (
   req: IncomingMessage
-): ReturnType<typeof generateButton | typeof notFoundHandler> => {
+): Promise<ReturnType<typeof generateButton | typeof notFoundHandler>> => {
+  const params = await getParams(req);
+
   if (req.method === "get" && req.url === "/generate-button") {
-    return generateButton();
+    return generateButton(params);
   }
 
   return notFoundHandler();
 };
 
 const notFoundHandler = () => ({ result: false });
+
+const getParams = async (req: IncomingMessage): Promise<any> => {
+  return new Promise((resolve) => {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      resolve(body);
+    });
+  });
+};
 
 server.listen(4000);
