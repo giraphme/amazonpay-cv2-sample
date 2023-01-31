48b17785f74e40c7544aff58972f9912c40fdb86
TamakiMaeda <giraph.me@gmail.com>
2023-01-31 10:48:22 +0900

    add link

--- src/generateButton.ts
+++ src/generateButton.ts
@@ -10,6 +10,7 @@ type Result = {
   signature: string;
 };
 
+// https://developer.amazon.com/ja/docs/amazon-pay-checkout/add-the-amazon-pay-button.html#2-checkoutsession-%E3%83%9A%E3%82%A4%E3%83%AD%E3%83%BC%E3%83%89%E3%82%92%E7%94%9F%E6%88%90%E3%81%97%E3%81%BE%E3%81%99
 export const generateButton = async ({
   callbackUrl,
 }: Params): Promise<Result> => {
--- src/getCheckoutSession.ts
+++ src/getCheckoutSession.ts
@@ -4,6 +4,7 @@ type Params = {
   checkoutSessionId: string;
 };
 
+// https://developer.amazon.com/ja/docs/amazon-pay-api-v2/checkout-session.html#get-checkout-session
 export const getCheckoutSession = async ({ checkoutSessionId }: Params) => {
   return request({
     method: "GET",
